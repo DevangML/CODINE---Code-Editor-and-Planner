0534cf72b02d699e8386e7bad1fb424213d1e391
Devang <devangmanjramkar@gmail.com>
2021-10-29 22:22:38 +0530

    17th commit

--- view/src/pages/Vanilla.js
+++ view/src/pages/Vanilla.js
@@ -7,24 +7,24 @@ import Dfooter from '../components/general/Dfooter'
 
 function Vanilla() {
   return (
-    <section className="vanilla">
-      <header className="vanilla__header">
-        <h1 className="vanilla__header__item">Vanilla Web Live Compiler</h1>
-      </header>
-      <Router>
-        <VanillaRouteButton />
-        <Switch>
-          <Route path="/nvanilla">
-            <VanillaNormal />
-          </Route>
-          <Route path="/fvanilla">
-            <VanillaFluent />
-          </Route>
-        </Switch>
-      </Router>
-      <Dfooter />
-    </section>
-  )
+			<section className='vanilla'>
+				<header className='vanilla__header'>
+					<h1 className='vanilla__header__item'>Vanilla Web Live Compiler</h1>
+				</header>
+				<Router>
+					<VanillaRouteButton />
+					<Switch>
+						<Route path='/nvanilla'>
+							<VanillaNormal />
+						</Route>
+						<Route path='/fvanilla'>
+							<VanillaFluent />
+						</Route>
+					</Switch>
+				</Router>
+				<Dfooter />
+			</section>
+	)
 }
 
 export default Vanilla
