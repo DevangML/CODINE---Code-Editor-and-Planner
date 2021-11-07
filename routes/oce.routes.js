2dc0c41a913739ac640c4fe4adfb10c0bd80424d
Devang Manjramkar <devangmanjramkar@gmail.com>
2021-11-08 00:02:26 +0530

    preact 2

--- routes/oce.routes.js
+++ routes/oce.routes.js
@@ -1,16 +1,10 @@
 const express = require('express')
 const {
   oceContactPostController,
-  oceVanillaController,
+  oceVanillaController
 } = require('../controllers/oce.controllers')
 const oceContactRouter = express.Router()
 const oceVanillaRouter = express.Router()
-const oceAuthRouter = express.Router()
-const oceProfileRouter = express.Router()
-const oceUserRouter = express.Router()
-const storage = require('../controllers/middlewares/oce.middlewares')
-const checkAuth = require('../controllers/middlewares/check-auth')
-const multer = require('multer')
 
 // Routes here
 
