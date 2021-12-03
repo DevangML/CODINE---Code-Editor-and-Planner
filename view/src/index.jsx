154ce52ac6c66cd6820487c3c3f99e8ec0a0aa73
Devang Manjramkar <devangmanjramkar@gmail.com>
2021-12-03 18:29:18 +0530

    Minor Fixes

--- view/src/index.jsx
+++ view/src/index.jsx
@@ -1,1 +1,15 @@
-// hi
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './redux/store';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
+
+// serviceWorker.register();
--- view/src/redux/reducers/rootReducer.js
+++ view/src/redux/reducers/rootReducer.js
@@ -1,1 +1,14 @@
-// rootreducer
+import { combineReducers } from 'redux';
+import authReducer from './authReducers/authReducer';
+import boardReducer from './projectPlannerReducers/boardReducer';
+import cardsByIdReducer from './projectPlannerReducers/cardsByIdReducer';
+import listsByIdReducer from './projectPlannerReducers/listByIdReducer';
+
+const rootReducer = combineReducers({
+  board: boardReducer,
+  listsById: listsByIdReducer,
+  cardsById: cardsByIdReducer,
+  auth: authReducer,
+});
+
+export default rootReducer;
