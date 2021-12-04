66dfd90e148fe4fa7aa837c2a36971bdeda2c82f
Devang Manjramkar <devangmanjramkar@gmail.com>
2021-12-04 19:05:17 +0530

    making deployable 1.0

--- view/src/api/index.js
+++ view/src/api/index.js
@@ -2,28 +2,7 @@ import axios from 'axios';
 
 /*eslint-disable*/
 
-const API = axios.create({ baseURL: 'api' });
-
-export const compilerPost = () =>
-  API.post('/compiler/runCode', { language, code, input }).then((res) => {
-    if (res.data.memory && res.data.cpuTime) {
-      setOutputLogs('');
-      setOutputLogs(
-        `Memory Used: ${res.data.memory} \nCPU Time: ${res.data.cpuTime} \n${res.data.output} `
-      );
-    } else {
-      setOutputLogs(`${res.data.output} `);
-    }
-    setStat('Run');
-  });
-
-export const contactPost = () =>
-  API.post(
-    '/contact/post',
-    (contactData = { Name: name, Email: email, Phone: phone, Message: message })
-  ).then(() => {
-    alert('Thank You For Contacting Us');
-  });
+export const API = axios.create({ baseURL: 'https://codex7.herokuapp.com' });
 
 export const signin = (formData) => API.post('/user/signin', formData);
 export const signup = (formData) => API.post('/user/signin', formData);
