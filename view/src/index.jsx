f24531b621f51c7f53680268a15b24c753ca4582
Devang Manjramkar <devangmanjramkar@gmail.com>
2022-01-18 00:04:02 +0530

    Heroku Fix 4

--- view/src/index.jsx
+++ view/src/index.jsx
@@ -1,21 +1,21 @@
 import ReactDOM, { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
+// import * as Sentry from '@sentry/react';
+// import { Integrations } from '@sentry/tracing';
 
 import App from './App';
 import store from './redux/store';
 import './index.css';
 
-Sentry.init({
-  dsn: process.env.REACT_APP_SENTRY_DSN,
-  integrations: [new Integrations.BrowserTracing()],
+// Sentry.init({
+//   dsn: process.env.REACT_APP_SENTRY_DSN,
+//   integrations: [new Integrations.BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+//   // Set tracesSampleRate to 1.0 to capture 100%
+//   // of transactions for performance monitoring.
+//   // We recommend adjusting this value in production
+//   tracesSampleRate: 1.0,
+// });
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
