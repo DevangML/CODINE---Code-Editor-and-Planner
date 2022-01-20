c2258517eff758421da3e13e7ecc8141fd306f93
Devang Manjramkar <devangmanjramkar@gmail.com>
2022-01-20 17:41:08 +0530

    Host Fix 12

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -4,29 +4,25 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import Lottie from 'react-lottie';
+import BoxLoading from './domain/common/parts/BoxLoading';
 import { ToastContainer } from 'react-toastify';
 import { loadUser } from './redux/actions/authActions';
 import setAuthToken from './redux/utils/setAuthToken';
-import * as location from './1055-world-locations.json';
-import * as success from './1127-success.json';
 import 'react-toastify/dist/ReactToastify.css';
-
-//  <ErrorBoundry FallbackComponent={FallBackLayout} onError={errorHandler}>
 
 // Component Imports
 
 import Sidebar from './domain/navigation/Sidebar';
 const Vanilla = lazy(() => import('./domain/compilers/pages/vanilla/Vanilla'));
-const Home = lazy(() => import('./domain/user/pages/home/Home'));
+import Home from './domain/user/pages/home/Home';
 const ContactMe = lazy(() => import('./domain/user/pages/contactMe/ContactMe'));
 const ProjectPlanner = lazy(() => import('./domain/project/pages/projectPlanner/ProjectPlanner'));
 const Compiler = lazy(() => import('./domain/compilers/pages/liveCompiler/Compiler'));
 import store from './redux/store';
-const Landing = lazy(() => import('./domain/layouts/Landing'));
-const LandingRouteButton = lazy(() => import('./domain/layouts/components/LandingRouteButton'));
-const Register = lazy(() => import('./domain/auth/pages/Register'));
-const Login = lazy(() => import('./domain/auth/pages/Login'));
+import Landing from './domain/layouts/Landing';
+import LandingRouteButton from './domain/layouts/components/LandingRouteButton';
+import Register from './domain/auth/pages/Register';
+import Login from './domain/auth/pages/Login';
 const Todos = lazy(() => import('./domain/project/pages/toDoList/Todos'));
 
 import { FallBackLayout } from './domain/layouts/FallBackLayout';
@@ -60,24 +56,6 @@ import { FallBackLayout } from './domain/layouts/FallBackLayout';
 
 // Loader Section
 
-const defaultOptions1 = {
-  loop: true,
-  autoplay: true,
-  animationData: location.default,
-  rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice',
-  },
-};
-
-const defaultOptions2 = {
-  loop: true,
-  autoplay: true,
-  animationData: success.default,
-  rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice',
-  },
-};
-
 // Auth Section
 if (store.getState().auth.token) {
   setAuthToken(store.getState().auth.token);
@@ -90,6 +68,10 @@ const App = ({ isAuthenticated }) => {
   const [completed, setcompleted] = useState(undefined);
   const dispatch = useDispatch();
 
+  // const mystyle = {
+  //   fontSize: '5vh',
+  // };
+
   useEffect(() => {
     setTimeout(() => {
       fetch('https://jsonplaceholder.typicode.com/posts')
@@ -100,7 +82,7 @@ const App = ({ isAuthenticated }) => {
 
           setTimeout(() => {
             setcompleted(true);
-          }, 1000);
+          }, 2000);
         })
         .catch((err) => console.log(err));
     }, 2000);
@@ -119,9 +101,29 @@ const App = ({ isAuthenticated }) => {
       {!completed ? (
         <section className='loading'>
           {!loading ? (
-            <Lottie options={defaultOptions1} height={700} width={700} />
+            <BoxLoading />
           ) : (
-            <Lottie options={defaultOptions2} height={400} width={400} />
+            <svg version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 130.2 130.2'>
+              <circle
+                class='path circle'
+                fill='none'
+                stroke='#73AF55'
+                stroke-width='6'
+                stroke-miterlimit='10'
+                cx='65.1'
+                cy='65.1'
+                r='62.1'
+              />
+              <polyline
+                class='path check'
+                fill='none'
+                stroke='#73AF55'
+                stroke-width='6'
+                stroke-linecap='round'
+                stroke-miterlimit='10'
+                points='100.2,40.2 51.5,88.8 29.8,67.5 '
+              />
+            </svg>
           )}
         </section>
       ) : (
---
+++ view/src/domain/common/styles/partStyles/loaderCommon.js
@@ -0,0 +1,28 @@
+const commonStyle = {
+  margin: 'auto',
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+};
+
+const sizeItem = {
+  small: '10vh',
+  default: '12vh',
+  large: '14vh',
+};
+
+const sizeContainer = {
+  small: '16vh',
+  default: '18vh',
+  large: '20vh',
+};
+
+const borderRadiusContainerSize = {
+  small: '1vh',
+  default: '1.5vh',
+  large: '1.8vh',
+};
+
+export { commonStyle, sizeItem, sizeContainer, borderRadiusContainerSize };
